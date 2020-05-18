fac0cf7819390b371ddf35ea1a63b984da98c654
Ilya Tyutin <emmarrgghh@gmail.com>
2020-05-18 09:59:28 +0300

    exclude tests etc

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(),
+    packages=find_packages(exclude=("*.tests", "*.tests.*", "tests.*", "tests", "*.examples", "*.examples.*", "examples.*", "examples")),
     include_package_data=True,
     install_requires=["pandas"],
 )
